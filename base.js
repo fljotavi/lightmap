11e616ab46a05e43e6e0546292779ed4e4ac7c6e
Tzingtao Chow <Cyphexl@outlook.com>
2017-10-18 23:06:44 +0800

    trying to get geoLocation

--- base.js
+++ base.js
@@ -11,6 +11,16 @@ function hidePage() {
     shadePanel.fadeIn();
 }
 
+function geoOnSuccess(pos) {
+    var crd = pos.coords;
+    iqwerty.toast.Toast(crd.latitude);
+}
+
+function geoOnError(pos) {
+    var crd = pos.coords;
+    iqwerty.toast.Toast(地理位置获取失败);
+}
+
 function toPage(currentPage, destination) {
     hidePage();
     setTimeout(function () {
@@ -18,13 +28,22 @@ function toPage(currentPage, destination) {
         mains[currentPage].hide();
         bgs[destination].show();
         mains[destination].show();
+
         if (destination == 1 && beenThere[destination] == false) {
             const mySiema = new Siema({
             });
         }
         if (destination == 2 && beenThere[destination] == false) {
+            
+            var geoOptions = {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0
+            };
+
             var seriesContainer = echarts.init(document.getElementById('c-map'));
             seriesContainer.setOption(option);
+            navigator.geolocation.getCurrentPosition(geoOnSuccess, geoOnError, geoOptions);
         }
         beenThere[destination] = true;
         revealPage();
