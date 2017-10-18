cf8d668dbf7fb75e2041bd656e3e0b964e623428
Tzingtao Chow <Cyphexl@outlook.com>
2017-10-18 14:09:55 +0800

    clipboard try

--- base.js
+++ base.js
@@ -1,7 +1,7 @@
 var shadePanel = $('#shade-panel');
 var bgs = [$('#bg-a'), $('#bg-b'), $('#bg-c')];
 var mains = [$('#main-a'), $('#main-b'), $('#main-c')];
-var beenThere = false;
+var beenThere = [true, false, false];
 
 function revealPage() {
     shadePanel.fadeOut();
@@ -18,15 +18,29 @@ function toPage(currentPage, destination) {
         mains[currentPage].hide();
         bgs[destination].show();
         mains[destination].show();
-        if(destination == 1 && beenThere == false) {
+        if (destination == 1 && beenThere[destination] == false) {
             const mySiema = new Siema({
             });
-            beenThere = true;
         }
+        if (destination == 2 && beenThere[destination] == false) {
+            var seriesContainer = echarts.init(document.getElementById('c-map'));
+            seriesContainer.setOption(option);
+        }
+        beenThere[destination] = true;
         revealPage();
     }, 1000)
 }
 
 $('#a-button-self').click(function () {
     toPage(0,1);
+});
+
+$('#b-button').click(function () {
+    toPage(1,2);
+});
+
+$('#share-self').click(function () {
+    var clipBoardContent = document.location;
+    clipBoardContent += '\r\n';
+    window.clipboardData.setData("Text", clipBoardContent);
 });
